a398e4bed131f8c5acae7d581f4a50d47ddfebfd
skn-036 <noman0703036@gmail.com>
2023-08-30 01:38:58 +0600

    #4 resolve puppeteer options for ubuntu

--- src/controllers/pdf.controller.ts
+++ src/controllers/pdf.controller.ts
@@ -1,24 +1,25 @@
 import { Request, Response } from 'express';
-import puppeteer, { PDFOptions } from 'puppeteer';
+import { PDFOptions } from 'puppeteer';
 import path from 'path';
 import pdf2img from 'pdf-img-convert';
 import fs from 'fs';
 
+import { launchPuppeteer, closePuppeteer } from '@/utils/puppeteer';
+
 import { TemplateInformation } from '@/types/pdf';
 
 export const onPdfGeneration = async (req: Request, res: Response) => {
-    try {
-        const filePath = path.join(__dirname, '../public/pdf/output.pdf');
-        const pdf = await generatePdf(req, filePath);
-
-        res.set({
-            'Content-Type': 'application/pdf',
-            'Content-Disposition': 'attachment; filename="document.pdf"',
-            'Content-Length': pdf.length,
-        }).send(pdf);
-    } catch (error) {
-        res.send(error);
-    }
+    // try {
+    //     const filePath = path.join(__dirname, '../public/pdf/output.pdf');
+    //     const pdf = await generatePdf(req, filePath);
+    //     res.set({
+    //         'Content-Type': 'application/pdf',
+    //         'Content-Disposition': 'attachment; filename="document.pdf"',
+    //         'Content-Length': pdf.length,
+    //     }).send(pdf);
+    // } catch (error) {
+    //     res.send(error);
+    // }
 };
 
 export const onPdfPreview = async (req: Request, res: Response) => {
@@ -46,55 +47,59 @@ export const onPdfPreview = async (req: Request, res: Response) => {
 
         res.send(filePaths);
     } catch (error) {
-        res.send(error);
+        res.status(404).send(error);
     }
 };
 
 export const generatePdf = async (req: Request, filePath: string = '') => {
-    const { body, templateInformation } = req.body as {
-        body: string;
-        templateInformation: TemplateInformation;
-    };
+    try {
+        const { body, templateInformation } = req.body as {
+            body: string;
+            templateInformation: TemplateInformation;
+        };
 
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
+        const browser = await launchPuppeteer();
+        const page = await browser.newPage();
 
-    await page.setContent(body);
-    const header = `<header style="position: fixed; width: 100%; height: 100%; top: 0; left: 0"><img width: 100%; height: 100%;" src="${templateInformation.template.background}"><div>Test header</div></header>`;
+        await page.setContent(body);
+        const header = `<header style="position: fixed; width: 100%; height: 100%; top: 0; left: 0"><img width: 100%; height: 100%;" src="${templateInformation.template.background}"><div>Test header</div></header>`;
 
-    await page.addStyleTag({
-        content: `
-          html {
-            -webkit-print-color-adjust: exact !important;
-            -webkit-filter: opacity(1) !important;
-          }
-          @media print {
-            .wrapper-div {
-               break-inside: avoid;
-            }
-          }
-        `,
-    });
+        await page.addStyleTag({
+            content: `
+              html {
+                -webkit-print-color-adjust: exact !important;
+                -webkit-filter: opacity(1) !important;
+              }
+              @media print {
+                .wrapper-div {
+                   break-inside: avoid;
+                }
+              }
+            `,
+        });
 
-    const options: PDFOptions = {
-        format: 'A4',
-        printBackground: false,
-        displayHeaderFooter: true,
-        headerTemplate: header,
-        path: filePath
-            ? filePath
-            : path.resolve(__dirname, '../public/pdf/output.pdf'),
-        margin: {
-            top: 72,
-            bottom: 72,
-            left: 72,
-            right: 72,
-        },
-        scale: 1.33,
-    };
+        const options: PDFOptions = {
+            format: 'A4',
+            printBackground: false,
+            displayHeaderFooter: true,
+            headerTemplate: header,
+            path: filePath
+                ? filePath
+                : path.resolve(__dirname, '../public/pdf/output.pdf'),
+            margin: {
+                top: 72,
+                bottom: 72,
+                left: 72,
+                right: 72,
+            },
+            scale: 1.33,
+        };
 
-    const pdf = await page.pdf(options);
-    await browser.close();
+        const pdf = await page.pdf(options);
+        await closePuppeteer(browser);
 
-    return pdf;
+        return pdf;
+    } catch (error) {
+        console.log(error);
+    }
 };
